bdd3c7782c24688dde52c912403f88e52ab6d1f4
Max <mjvcarroll@gmail.com>
2025-11-27 21:35:09 +0000

    Switch thumb layout

--- keymap.c
+++ keymap.c
@@ -85,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_W,  KC_L,  KC_Y,  KC_P,  KC_B,                         KC_Z, KC_F,  KC_O,      KC_U,       KC_QUOTE,
         HRM_C, HRM_R, HRM_S, HRM_T, KC_G,                         KC_M, HRM_N, HRM_E,     HRM_I,      HRM_A,
         FN_Q,  KC_J,  KC_V,  KC_D,  KC_K,                         KC_X, KC_H,  KC_SLASH,  KC_COMMA,   KC_DOT,
-                                    KC_SPC, TMB_SYM,     TMB_NAV, KC_BSPC
+                                    TMB_SYM, KC_SPC,     KC_BSPC, TMB_NAV
     ),
 
     [_FN] = LAYOUT_split_3x5_2(KC_TRNS, KC_TRNS, KC_COLN, KC_ESC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL, KC_TRNS, KC_PERC, KC_SLSH, KC_ENT, KC_TRNS, DF(1), KC_LGUI, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_EXLM, KC_TRNS, DF(0), KC_TRNS, RALT_T(KC_COMM), RCTL_T(KC_DOT), QK_BOOT, KC_TRNS, KC_TAB, KC_NO, KC_TRNS)
